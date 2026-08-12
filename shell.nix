517fedafb4d8398c64a97ab5bf982d7c6b5e8b7b
Julien Moutinho <julm@sourcephile.fr>
2026-08-12 04:27:20 +0200

    +user@compatibility(flake-compat): init traditional layer

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  flake-lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  flake-compat-node = flake-lock.nodes.root.inputs.flake-compat;
+  flake-compat = builtins.fetchTarball {
+    url =
+      flake-lock.nodes.${flake-compat-node}.locked.url
+        or "https://github.com/NixOS/flake-compat/archive/${
+          flake-lock.nodes.${flake-compat-node}.locked.rev
+        }.tar.gz";
+    sha256 = flake-lock.nodes.${flake-compat-node}.locked.narHash;
+  };
+  flake = (
+    import flake-compat {
+      src = ./.;
+    }
+  );
+in
+flake.shellNix
