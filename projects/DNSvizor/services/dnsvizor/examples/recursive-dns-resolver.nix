ngi-nix/ngipkgs

projects/DNSvizor: init

projects/DNSvizor/services/dnsvizor/examples/recursive-dns-resolver.nix
@@ -1,0 +1,30 @@
+{ ... }:
+
+{
+  services.dnsvizor = {
+    enable = true;
+    memory = 32;
+    mainInterface = "enp0s3";
+    settings = {
+      hostname = "dnsvizor.mydomain.com";
+      ipv4 = "10.0.0.2/24";
+      ipv4-gateway = "10.0.0.1";
+      ipv6 = "fdc9:281f:4d7:9ee9::2/64";
+      ipv6-gateway = "fdc9:281f:4d7:9ee9::1";
+      ca-seed = "Te9ffyY3Clcaz/4P7eFLyZQfLWIz/fSSK4NDb8THMDc=";
+      password = "password";
+      dns-block = [
+        "block1.cli.example.com"
+        "block2.cli.example.com"
+      ];
+      dns-blocklist-url = [
+        "http://10.0.0.1/block-list-4"
+        "http://[fdc9:281f:4d7:9ee9::1]:80/block-list-6"
+        "https://example.com/non-existent-block-list"
+      ];
+      qname-minimisation = true;
+      opportunistic-tls-authoritative = true;
+    };
+    openFirewall = true;
+  };
+}

projects/DNSvizor/services/dnsvizor/examples/stub-dns-resolver.nix
@@ -1,0 +1,25 @@
+{ ... }:
+
+{
+  services.dnsvizor = {
+    enable = true;
+    memory = 32;
+    mainInterface = "enp0s3";
+    settings = {
+      ipv4 = "10.0.0.2/24";
+      ipv4-gateway = "10.0.0.1";
+      ipv4-only = "true";
+      ca-seed = "Te9ffyY3Clcaz/4P7eFLyZQfLWIz/fSSK4NDb8THMDc=";
+      password = "password";
+      dns-block = [
+        "block1.cli.example.com"
+        "block2.cli.example.com"
+      ];
+      dns-blocklist-url = [
+        "http://10.0.0.1/block-list"
+        "https://example.com/non-existent-block-list"
+      ];
+      dns-upstream = "tls:1.1.1.1";
+    };
+  };
+}

ju1m/mirage-nix

projects/DNSvizor/services/dnsvizor/examples/recursive-dns-resolver.nix
@@ -1,0 +1,30 @@
+{ ... }:
+
+{
+  services.dnsvizor = {
+    enable = true;
+    memory = 128;
+    mainInterface = "enp1s0";
+    settings = {
+      hostname = "dnsvizor.mydomain.example";
+      ipv4 = "10.0.0.2/24";
+      ipv4-gateway = "10.0.0.1";
+      ipv6 = "fdc9:281f:4d7:9ee9::2/64";
+      ipv6-gateway = "fdc9:281f:4d7:9ee9::1";
+      ca-seed = "Te9ffyY3Clcaz/4P7eFLyZQfLWIz/fSSK4NDb8THMDc=";
+      password = "password";
+      dns-block = [
+        "block1.cli.example.com"
+        "block2.cli.example.com"
+      ];
+      dns-blocklist-url = [
+        "http://10.0.0.1/block-list-4"
+        "http://[fdc9:281f:4d7:9ee9::1]:80/block-list-6"
+        "https://example.com/non-existent-block-list"
+      ];
+      qname-minimisation = true;
+      opportunistic-tls-authoritative = true;
+    };
+    openFirewall = true;
+  };
+}

projects/DNSvizor/services/dnsvizor/examples/stub-dns-resolver.nix
@@ -1,0 +1,25 @@
+{ ... }:
+
+{
+  services.dnsvizor = {
+    enable = true;
+    memory = 128;
+    mainInterface = "enp1s0";
+    settings = {
+      ipv4 = "10.0.0.2/24";
+      ipv4-gateway = "10.0.0.1";
+      ipv4-only = "true";
+      ca-seed = "Te9ffyY3Clcaz/4P7eFLyZQfLWIz/fSSK4NDb8THMDc=";
+      password = "password";
+      dns-block = [
+        "block1.cli.example.com"
+        "block2.cli.example.com"
+      ];
+      dns-blocklist-url = [
+        "http://10.0.0.1/block-list"
+        "https://example.com/non-existent-block-list"
+      ];
+      dns-upstream = "tls:1.1.1.1";
+    };
+  };
+}